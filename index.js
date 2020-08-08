c5494f814d61d4a545dec891adddf02a32ec4ced
Jason Lengstorf <jason@lengstorf.com>
2020-08-07 23:22:29 -0700

    feat: first rule

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 module.exports = {
   rules: {
-    'no-template-literals': {
+    'use-triple-equals': {
       create: function (context) {
         return {
-          TemplateLiteral(node) {
-            context.report(node, 'Do not use template literals');
+          BinaryExpression(node) {
+            if (node.operator === '!=' || node.operator === '==') {
+              context.report({
+                node,
+                message: `ACK-tually, you should use ${node.operator}= because ${node.operator} is buggy.`,
+              });
+            }
+          },
+        };
+      },
+    },
+    'use-double-equals': {
+      create: function (context) {
+        return {
+          BinaryExpression(node) {
+            if (node.operator === '!==' || node.operator === '===') {
+              context.report({
+                node,
+                message: `ACK-tually, you should use ${node.operator.substr(
+                  0,
+                  2,
+                )} because ${node.operator} is overkill.`,
+              });
+            }
+          },
+        };
+      },
+    },
+    'async-func-name': {
+      create: function (context) {
+        return {
+          FunctionDeclaration(node) {
+            if (node.async && !/Async$/.test(node.id.name)) {
+              context.report({
+                node,
+                message: "Async function name must end in 'Async'",
+              });
+            }
           },
         };
       },
