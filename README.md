83ab96afedace05ba77c9a7732b743bae53d1f88
Jason Lengstorf <jason@lengstorf.com>
2020-08-08 14:15:01 -0700

    feat: add a preset

--- README.md
+++ README.md
@@ -12,11 +12,16 @@ npm install --save-dev eslint-plugin-replyguy
 
 ```js
 module.exports = {
-  parserOptions: { ecmaVersion: 2018 },
-  plugins: ['replyguy'],
-  rules: {
-    'replyguy/use-triple-equals': 2,
-    'replyguy/use-double-equals': 2,
-  },
+  extends: ['plugin:replyguy/all'],
 };
 ```
+
+## Why You Should Use This Plugin
+
+If you want your development experience to closely mirror what it’s like to share code on Twitter, you’ll love this ESLint config!
+
+The rules in this repo are mostly untested and largely based on something read somewhere once and half-remembered but asserted as Absolute Truth™.
+
+## Contributions Welcome
+
+Please submit your favorite contradictory opinions as enforceable rules. Only changes that result in circular errors will be merged. But don’t you _dare_ submit tests or types or something that would make this repo look like quality software.
--- index.js
+++ index.js
@@ -1,35 +1,20 @@
+const rules = {
+  'no-loops': require('./lib/rules/no-loops'),
+  'no-array-methods': require('./lib/rules/no-array-methods'),
+  'use-triple-equals': require('./lib/rules/use-triple-equals'),
+  'use-double-equals': require('./lib/rules/use-double-equals'),
+};
+
+const rulesAsErrors = Object.fromEntries(
+  Object.keys(rules).map((key) => [`replyguy/${key}`, 2]),
+);
+
 module.exports = {
-  rules: {
-    'use-triple-equals': {
-      create: function (context) {
-        return {
-          BinaryExpression(node) {
-            if (node.operator === '!=' || node.operator === '==') {
-              context.report({
-                node,
-                message: `ACK-tually, you should use ${node.operator}= because ${node.operator} is buggy.`,
-              });
-            }
-          },
-        };
-      },
-    },
-    'use-double-equals': {
-      create: function (context) {
-        return {
-          BinaryExpression(node) {
-            if (node.operator === '!==' || node.operator === '===') {
-              context.report({
-                node,
-                message: `ACK-tually, you should use ${node.operator.substr(
-                  0,
-                  2,
-                )} because ${node.operator} is overkill.`,
-              });
-            }
-          },
-        };
-      },
+  rules,
+  configs: {
+    all: {
+      plugins: ['replyguy'],
+      rules: rulesAsErrors,
     },
   },
 };
