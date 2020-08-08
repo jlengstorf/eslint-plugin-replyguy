b7652d21411ac176c205692f07ee6e00dcf22c03
Jason Lengstorf <jason@lengstorf.com>
2020-08-07 19:17:59 -0700

    wip: how does this work?

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  rules: {
+    'no-template-literals': {
+      create: function (context) {
+        return {
+          TemplateLiteral(node) {
+            context.report(node, 'Do not use template literals');
+          },
+        };
+      },
+    },
+  },
+};
