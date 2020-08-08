88cf3454ca80206a914ce2c4cf26c6b667048fe9
Jason Lengstorf <jason@lengstorf.com>
2020-08-07 23:41:32 -0700

    fix: remove additional rule

--- index.js
+++ index.js
@@ -31,19 +31,5 @@ module.exports = {
         };
       },
     },
-    'async-func-name': {
-      create: function (context) {
-        return {
-          FunctionDeclaration(node) {
-            if (node.async && !/Async$/.test(node.id.name)) {
-              context.report({
-                node,
-                message: "Async function name must end in 'Async'",
-              });
-            }
-          },
-        };
-      },
-    },
   },
 };
