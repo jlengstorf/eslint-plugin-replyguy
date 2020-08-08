68821960683a0ba21c990266ec48971abbf0e46d
laurie <laurie@tenmilesquare.com>
2020-08-08 18:46:50 -0400

    no ternaries

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const rules = {
   'use-double-equals': require('./lib/rules/use-double-equals'),
   'i-have-a-daughter': require('./lib/rules/i-have-a-daughter'),
   'no-let-const': require('./lib/rules/no-let-const'),
+  'no-ternary': require('./lib/rules/no-ternary'),
 }
 
 const rulesAsErrors = Object.fromEntries(
