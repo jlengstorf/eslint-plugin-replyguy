c3a0e94d08b771854880aeca57cf0cc602d004c4
laurie <laurie@tenmilesquare.com>
2020-08-08 18:25:58 -0400

    let vs const

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const rules = {
   'use-triple-equals': require('./lib/rules/use-triple-equals'),
   'use-double-equals': require('./lib/rules/use-double-equals'),
   'i-have-a-daughter': require('./lib/rules/i-have-a-daughter'),
-};
+  'no-const': require('./lib/rules/no-let-const'),
+}
 
 const rulesAsErrors = Object.fromEntries(
-  Object.keys(rules).map((key) => [`replyguy/${key}`, 2]),
-);
+  Object.keys(rules).map((key) => [`replyguy/${key}`, 2])
+)
 
 module.exports = {
   rules,
@@ -18,4 +19,4 @@ module.exports = {
       rules: rulesAsErrors,
     },
   },
-};
+}
