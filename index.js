7da1ac5168135298ba15c0fb83b3a5fcf418fd01
Alaa Zorkane <alaazorkane@gmail.com>
2020-08-09 01:05:44 +0100

    feat: prettier

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const rules = {
-  "no-loops": require("./lib/rules/no-loops"),
-  "no-array-methods": require("./lib/rules/no-array-methods"),
-  "use-triple-equals": require("./lib/rules/use-triple-equals"),
-  "use-double-equals": require("./lib/rules/use-double-equals"),
-  "i-have-a-daughter": require("./lib/rules/i-have-a-daughter"),
-  "no-let-const": require("./lib/rules/no-let-const"),
-  "no-ternary": require("./lib/rules/no-ternary"),
-  "no-if": require("./lib/rules/no-if"),
-};
+  'no-loops': require('./lib/rules/no-loops'),
+  'no-array-methods': require('./lib/rules/no-array-methods'),
+  'use-triple-equals': require('./lib/rules/use-triple-equals'),
+  'use-double-equals': require('./lib/rules/use-double-equals'),
+  'i-have-a-daughter': require('./lib/rules/i-have-a-daughter'),
+  'no-let-const': require('./lib/rules/no-let-const'),
+  'no-ternary': require('./lib/rules/no-ternary'),
+  'no-if': require('./lib/rules/no-if'),
+}
 
 const rulesAsErrors = Object.fromEntries(
   Object.keys(rules).map((key) => [`replyguy/${key}`, 2])
-);
+)
 
 module.exports = {
   rules,
   configs: {
     all: {
-      plugins: ["replyguy"],
+      plugins: ['replyguy'],
       rules: rulesAsErrors,
     },
   },
-};
+}
