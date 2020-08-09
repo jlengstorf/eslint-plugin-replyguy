de9506f4634cdb1d3d59beb13c7e5d1d6386fdbf
Manuel Cepeda <mecm1993@gmail.com>
2020-08-09 17:33:29 -0400

    feat: no test

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const rules = {
   'no-arrow-functions': require('./lib/rules/no-arrow-functions'),
   'no-var': require('./lib/rules/no-var'),
   'no-functions': require('./lib/rules/no-functions'),
-  'no-classes': require('./lib/rules/no-classes')
+  'no-classes': require('./lib/rules/no-classes'),
+  'no-tests': require('./lib/rules/no-test'),
 }
 
 const rulesAsErrors = Object.fromEntries(
