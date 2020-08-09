81776fff723856c6618baa678b0f0c37da63a571
Hebilicious <xsh4k3@gmail.com>
2020-08-09 01:43:43 +0100

    feat: add no class rule (#6)
    Co-authored-by: Jason Lengstorf <jason@lengstorf.com>

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const rules = {
   'no-regular-functions': require('./lib/rules/no-regular-functions'),
   'no-arrow-functions': require('./lib/rules/no-arrow-functions'),
   'no-var': require('./lib/rules/no-var'),
+  'no-functions': require('./lib/rules/no-functions'),
+  'no-classes': require('./lib/rules/no-classes')
 }
 
 const rulesAsErrors = Object.fromEntries(
@@ -21,7 +23,7 @@ module.exports = {
   configs: {
     all: {
       plugins: ['replyguy'],
-      rules: rulesAsErrors,
-    },
-  },
+      rules: rulesAsErrors
+    }
+  }
 }
