18178ed43e49073e5ea9a54931eb83a679be7afe
Judicael <46365844+judicaelandria@users.noreply.github.com>
2020-08-11 00:07:20 +0300

    feat: use and don't use jsx (#10)
    Co-authored-by: Judicael <jmanambintsoa@gmail.com>

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const rules = {
   'no-var': require('./lib/rules/no-var'),
   'no-functions': require('./lib/rules/no-functions'),
   'no-classes': require('./lib/rules/no-classes'),
+  'jsx-filename-extension': require('./lib/rules/jsx-filename-extension'),
   'no-tests': require('./lib/rules/no-test'),
   'no-light-theme': require('./lib/rules/no-light-theme'),
   'wrong-font-choice': require('./lib/rules/wrong-font-choice'),
